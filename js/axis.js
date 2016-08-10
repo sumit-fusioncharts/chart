2aeb8cfbf6b98e134a6a255649f22258e08a86a9
Sumit Pal <sumit@fusioncharts.com>
2016-08-11 04:17:15 +0530

    Axis function modified

--- js/axis.js
+++ js/axis.js
@@ -47,6 +47,7 @@ Axis.prototype.drawLabels = function(_width,_textArr,_x,_y,_id,_posTexts,_pos,_r
 		pos,
 		textVal,
 		i;
+
 	if(Array.isArray(_textArr)==true && _width>0){//work with text array
 		lenArr = _textArr.length;
 		space = _width/lenArr;
@@ -68,9 +69,40 @@ Axis.prototype.drawLabels = function(_width,_textArr,_x,_y,_id,_posTexts,_pos,_r
 		this.canvas.createText(svg,_x,_y,_textArr,textColor,fontSize,_posTexts,id);
 	}
 };
-Axis.prototype.lines = function(){
-	this.canvas.createLines();
+Axis.prototype.drawBoxes = function(_width,_height,_x,_y,_id){
+	var axis = this,
+		svg = axis && axis.element,
+		canvas = axis && axis.canvas,
+		rectId = _id,
+		rectClass = rectId+"class";
+		return canvas.createrect(svg,_x,_y,_height,_width,);
 };
-Axis.prototype.boxes = function(){
-	this.canvas.createrect();
+Axis.prototype.drawLines = function(_width,_numTicks,_x,_y,_isVertical,_id,_posTicks){
+	if(_numTicks<1){
+		return;
+	}
+	var axis = this,
+		svg = axis && axis.element,
+		canvas = axis && axis.canvas,
+		i,
+		space = _width/_numTicks,
+		posTicks = space*_posTicks,
+		tmpSpace,
+		lineId = _id,
+		classname = _id+"Class",
+		tickPos1,
+
+	if(_isVertical){
+		tickPos1 = _y;
+		for(i = 0; i<_numTicks+1; i++){//y fixed x changes
+			tmpSpace = space*i+posTicks;
+			canvas.createLines(svg,tmpSpace,tickPos1,tmpSpace,tickPos1,classname,lineId);
+		}
+	}else{
+		for(i = 0; i<_numTicks+1; i++){//x fixed y changes
+			tickPos1 = _x;
+			tmpSpace = space*i+posTicks;
+			canvas.createLines(svg,tickPos1,tmpSpace,tickPos1,tmpSpace,classname,lineId);
+		}
+	}	
 };
--- js/crosstab.js
+++ js/crosstab.js
@@ -28,36 +28,9 @@ Crosstab.prototype.draw = function(){
 		axis.drawTicks(20,svgW,lenZones,0,40,true,"verticalTicks",1);
 		axis.drawTicks(20,svgH,lenProducts,30,40,false,"verticalTicks",1);console.log(model);
 		axis.drawLabels(svgW,zones,0,30,"headerText","middle",0.5,true);
+		axis.drawLines(svgW,lenProducts,40,40,false,"xlines",1);
 
 };
 Crosstab.prototype.init = function(_w,_h,_canvas,_divId){
 	return _canvas.createSvg(_w,_h,"outerSvg","outerSvgClass",_divId);
-};
-Crosstab.prototype.header = function(_canvas,_svg,_draw){
-	if(!_draw){
-		return;
-	}
-	var crosstab = this,
-		jsonData = crosstab && crosstab.jsonData,
-		model = jsonData && jsonData.model,
-		zones = model && model.zones,
-		lenZones = zones.length,
-		svgDetails = jsonData && jsonData.svg,
-		barMaxW = svgDetails.barMaxW,
-		temp1,
-		temp2,
-		_top = 30,
-		_fontsize = 17,
-		_textPos = "middle",
-		_textClass = "headerText",
-		_color = "#000",
-		i;
-
-	for(i = 0; i < lenZones; i++){
-		temp1 = barMaxW*i;
-		temp2 = barMaxW/2;
-		temp1 = temp1 + temp2;
-		_canvas.createText(_svg,temp1,_top,zones[i],_color,_fontsize,_textPos,_textClass);
-	}
-
 };
